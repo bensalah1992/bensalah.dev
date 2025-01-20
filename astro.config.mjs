f885bb9fa4df3b00f602e0f6c3b68f3b12323ff0
offline <a.bensalah.net@gmail.com>
2025-01-20 19:42:12 +0100

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  // Make sure there's nothing here that might interfere with CSS processing
+});
